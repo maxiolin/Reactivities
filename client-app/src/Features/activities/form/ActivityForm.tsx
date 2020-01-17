5f54e806b083fdf0e9b1f829dd67a1e11526c490
Marco <sl_sv@msn.com>
2020-01-17 17:39:48 -0600

    Commit before deletion

--- client-app/src/Features/activities/form/ActivityForm.tsx
+++ client-app/src/Features/activities/form/ActivityForm.tsx
@@ -1,89 +1,144 @@
-import React, {useState, FormEvent, useContext, useEffect} from 'react'
-import { Segment, Form, Button, Grid } from 'semantic-ui-react'
-import ActivityStore from '../../../App/stores/activityStore'
-import { IActivity } from '../../../App/models/activity'
-import {v4 as uuid} from 'uuid'
-import { observer } from 'mobx-react-lite'
-import { RouteComponentProps } from 'react-router-dom'
+import React, { useState, FormEvent, useContext, useEffect } from "react";
+import { Segment, Form, Button, Grid, TextArea } from "semantic-ui-react";
+import ActivityStore from "../../../App/stores/activityStore";
+import { IActivity } from "../../../App/models/activity";
+import { v4 as uuid } from "uuid";
+import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router-dom";
+import { Form as FinalForm, Field } from "react-final-form";
+import TextInput from "../../../App/common/form/TextInput";
+import TextAreaInput from "../../../App/common/form/TextAreaInput";
+import SelectInput from "../../../App/common/form/SelectInput";
+import { category } from "../../../App/common/options/categoryOption";
 
 interface DetailParams {
-    id: string;
+  id: string;
 }
 
 const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
-    match,
-    history
+  match,
+  history
 }) => {
-    const activityStore = useContext(ActivityStore);
-    const {
-        createActivity, 
-        activity: initialState, 
-        editActivity, 
-        submitting, 
-        loadActivity,
-        clearActivity
-    } = activityStore;
-    
-    
+  const activityStore = useContext(ActivityStore);
+  const {
+    createActivity,
+    activity: initialState,
+    editActivity,
+    submitting,
+    loadActivity,
+    clearActivity
+  } = activityStore;
 
-    const [activity, setActivity] = useState<IActivity>({
-        id: '',
-        title:'',
-        category: '',
-        description: '',
-        date: '',
-        city: '',
-        venue: ''
-    });
+  const [activity, setActivity] = useState<IActivity>({
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: ""
+  });
 
-    useEffect(() => {
-        if(match.params.id && activity.id.length === 0) {
-            loadActivity(match.params.id).then(
-                () => initialState && setActivity(initialState)
-            );
-        }
-
-        return () => {
-            clearActivity();
-        }
-    }, [loadActivity, match.params.id, clearActivity, initialState, activity.id.length])
-
-    const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const {name, value} = event.currentTarget
-        setActivity({...activity, [name]: value})
+  useEffect(() => {
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialState && setActivity(initialState)
+      );
     }
 
-    const handleSubmit = () => {
-        if(activity.id.length === 0) {
-            let newActivity = {
-                ...activity,
-                id: uuid()
-            }
-            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
-        } else {
-            editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
-        }
-    }
-    
-    return (    
-        <Grid>
-            <Grid.Column width={10}>
-            <Segment clearing>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Input name='title' onChange={handleInputChange} placeholder='Title' value={activity.title}/>
-                    <Form.TextArea name='description' onChange={handleInputChange} rows={2} placeholder='Description'value={activity.description}/>
-                    <Form.Input name='category' onChange={handleInputChange} placeholder='Category' value={activity.category}/>
-                    <Form.Input name='date' onChange={handleInputChange} type="datetime-local" placeholder='Date' value={activity.date}/>
-                    <Form.Input name='city' onChange={handleInputChange} placeholder='City' value={activity.city}/>
-                    <Form.Input name='venue' onChange={handleInputChange} placeholder='Venue' value={activity.venue}/>
-                    <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-                    <Button onClick={() => history.push('/activities')}  floated='right' type='button' content='Cancel' />
-                </Form>
-            </Segment>
-            </Grid.Column>
-        </Grid>
-        
-    )
-}
+    return () => {
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    match.params.id,
+    clearActivity,
+    initialState,
+    activity.id.length
+  ]);
+
+  // const handleSubmit = () => {
+  //     if(activity.id.length === 0) {
+  //         let newActivity = {
+  //             ...activity,
+  //             id: uuid()
+  //         }
+  //         createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+  //     } else {
+  //         editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+  //     }
+  // }
+
+  const handleFinalFormSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <Segment clearing>
+          <FinalForm
+            onSubmit={handleFinalFormSubmit}
+            render={({ handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  name="title"
+                  placeholder="Title"
+                  value={activity.title}
+                  component={TextInput}
+                />
+                <Field
+                  name="description"
+                  placeholder="Description"
+                  rows={3}
+                  value={activity.description}
+                  component={TextAreaInput}
+                />
+                <Field
+                  name="category"
+                  placeholder="Category"
+                  value={activity.category}
+                  component={SelectInput}
+                  options={category}
+                />
+                <Field
+                  name="date"
+                  placeholder="Date"
+                  value={activity.date}
+                  component={TextInput}
+                />
+                <Field
+                  name="city"
+                  placeholder="City"
+                  value={activity.city}
+                  component={TextInput}
+                />
+                <Field
+                  name="venue"
+                  placeholder="Venue"
+                  value={activity.venue}
+                  component={TextInput}
+                />
+                <Button
+                  loading={submitting}
+                  floated="right"
+                  positive
+                  type="submit"
+                  content="Submit"
+                />
+                <Button
+                  onClick={() => history.push("/activities")}
+                  floated="right"
+                  type="button"
+                  content="Cancel"
+                />
+              </Form>
+            )}
+          />
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default observer(ActivityForm);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import {Router} from 'react-router-dom';
 import {createBrowserHistory} from 'history'
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-widgets/dist/css/react-widgets.css';
 import './App/Layout/styles.css';
 import App from './App/Layout/App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './App/Layout/ScrollToTop';
+import dateFnsLocalizer from 'react-widgets-date-fns';
 
+dateFnsLocalizer();
+    
 export const history = createBrowserHistory();
 
 ReactDOM.render(
